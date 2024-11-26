ca32ab84749bf8bdd2ba0b0559ab925e8b0b8747
HassanBahati <mukisabahati@gmail.com>
2024-11-26 09:09:16 +0300

    chore: add query client utils

---
+++ packages/react/utils.tsx
@@ -0,0 +1,19 @@
+import React, { type ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+    mutations: {
+      retry: false,
+    },
+  },
+});
+
+const wrapper = ({ children }: { children: ReactNode }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
+
+export { wrapper, queryClient };
