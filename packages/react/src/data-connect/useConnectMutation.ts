d3ae0fc28eb6b7e817bcbeeb4d8bec851500e96e
HassanBahati <mukisabahati@gmail.com>
2024-12-07 14:53:09 +0300

    feat(useConnectMutation): implement invalidate option

--- packages/react/src/data-connect/useConnectMutation.ts
+++ packages/react/src/data-connect/useConnectMutation.ts
@@ -1,9 +1,24 @@
-import { useMutation, type UseMutationResult } from "@tanstack/react-query";
+import {
+  useMutation,
+  useQueryClient,
+  type UseMutationOptions,
+  type UseMutationResult,
+} from "@tanstack/react-query";
+import { type FirebaseError } from "firebase/app";
 import {
   executeMutation,
   type MutationRef,
   type DataConnect,
+  type QueryRef,
 } from "firebase/data-connect";
+
+type UseConnectMutationOptions<
+  TData = unknown,
+  TError = FirebaseError,
+  Variables = unknown
+> = Omit<UseMutationOptions<TData, TError, Variables>, "mutationFn"> & {
+  invalidate?: QueryRef<unknown, unknown>[];
+};
 
 export function useConnectMutation<
   Fn extends (...args: any[]) => MutationRef<any, any>,
@@ -16,8 +31,25 @@ export function useConnectMutation<
     : Fn extends (vars: infer V) => MutationRef<any, any>
     ? V
     : never
->(ref: Fn): UseMutationResult<Data, Error, Variables> {
-  return useMutation<Data, Error, Variables>({
+>(
+  ref: Fn,
+  options?: UseConnectMutationOptions<Data, FirebaseError, Variables>
+): UseMutationResult<Data, FirebaseError, Variables> {
+  const queryClient = useQueryClient();
+
+  return useMutation<Data, FirebaseError, Variables>({
+    ...options,
+    onSuccess(...args) {
+      if (options?.invalidate && options.invalidate.length) {
+        for (const ref of options.invalidate) {
+          queryClient.invalidateQueries({
+            queryKey: [ref.name, ref.variables],
+          });
+        }
+      }
+
+      options?.onSuccess?.(...args);
+    },
     mutationFn: async (variables: Variables) => {
       const { data } = await executeMutation<Data, Variables>(ref(variables));
       return data;
