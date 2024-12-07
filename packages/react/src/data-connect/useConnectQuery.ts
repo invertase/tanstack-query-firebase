d61f99b4a3535e1782f49a56088921add8706ca5
HassanBahati <mukisabahati@gmail.com>
2024-12-07 14:39:23 +0300

    feat(useConnectQuery): enable passing query options

--- packages/react/src/data-connect/useConnectQuery.ts
+++ packages/react/src/data-connect/useConnectQuery.ts
@@ -1,14 +1,39 @@
-import { useQuery } from "@tanstack/react-query";
-import { type QueryRef, executeQuery } from "firebase/data-connect";
+import { useQuery, type UseQueryOptions } from "@tanstack/react-query";
+import { type FirebaseError } from "firebase/app";
+import {
+  type QueryRef,
+  type QueryResult,
+  executeQuery,
+} from "firebase/data-connect";
+
+type UseConnectQueryOptions<TData = unknown, TError = Error> = Omit<
+  UseQueryOptions<TData, TError>,
+  "queryFn"
+>;
 
 export function useConnectQuery<
   Data extends Record<string, any>,
   Variables = unknown
->(ref: QueryRef<Data, Variables>) {
-  return useQuery<Data, Error>({
-    queryKey: [ref.name],
+>(
+  refOrResult: QueryRef<Data, Variables> | QueryResult<Data, Variables>,
+  options?: UseConnectQueryOptions<Data, FirebaseError>
+) {
+  let queryRef: QueryRef<Data, Variables>;
+  let initialData: Data | undefined;
+
+  if ("ref" in refOrResult) {
+    queryRef = refOrResult.ref;
+    initialData = refOrResult.data;
+  } else {
+    queryRef = refOrResult;
+  }
+
+  return useQuery<Data, FirebaseError>({
+    initialData,
+    ...options,
+    queryKey: options?.queryKey ?? [queryRef.name, queryRef.variables],
     queryFn: async () => {
-      const { data } = await executeQuery<Data, Variables>(ref);
+      const { data } = await executeQuery<Data, Variables>(queryRef);
       return data;
     },
   });
