e2d90bdff6296012dc14c6a4e141ef2b8f4705dd
HassanBahati <mukisabahati@gmail.com>
2024-12-24 02:08:28 +0300

    fix(bug): react imported as a type

--- packages/react/src/auth/useSendSignInLinkToEmailMutation.test.tsx
+++ packages/react/src/auth/useSendSignInLinkToEmailMutation.test.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { act, renderHook, waitFor } from "@testing-library/react";
-import type React from "react";
+import React from "react";
 import { beforeEach, describe, expect, test } from "vitest";
 import { auth, wipeAuth } from "~/testing-utils";
 import { useSendSignInLinkToEmailMutation } from "./useSendSignInLinkToEmailMutation";
--- packages/react/src/auth/useSignInAnonymouslyMutation.test.tsx
+++ packages/react/src/auth/useSignInAnonymouslyMutation.test.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { act, renderHook, waitFor } from "@testing-library/react";
-import type React from "react";
+import React from "react";
 import {
 	type MockInstance,
 	afterEach,
