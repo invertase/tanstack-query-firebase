c348aa4c9ec2291ac3f2be031536f6f1c8385d26
Elliot Hesp <elliot.hesp@gmail.com>
2025-03-04 09:35:48 +0000

    chore: Simplify build flow

--- packages/angular/copyToBuild.js
+++
@@ -1,8 +0,0 @@
-import * as fs from 'node:fs';
-
-if(!fs.existsSync('./dist')) {
-    fs.mkdirSync('./dist');
-}
-fs.copyFileSync('./package.json', './dist/package.json');
-fs.copyFileSync('./README.md', './dist/README.md');
-fs.copyFileSync('./LICENSE', './dist/LICENSE');
