743f41b4ba2ec588e59556f9f913580ac0ecfd0e
Maneesh Tewani <mtewani@google.com>
2025-03-19 11:39:24 -0700

    Fix react typings

--- packages/react/src/data-connect/index.ts
+++ packages/react/src/data-connect/index.ts
@@ -1,4 +1,3 @@
-import { ReservedQueryKeys } from "./types";
 
 export {
   DataConnectQueryClient,
@@ -12,4 +11,4 @@ export {
   useDataConnectMutation,
   type useDataConnectMutationOptions,
 } from "./useDataConnectMutation";
-export type { FlattenedQueryResult, FlattenedMutationResult } from "./types";
+export type { QueryResultRequiredRef, UseDataConnectMutationResult, UseDataConnectQueryResult } from "./types";
