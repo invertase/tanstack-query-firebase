2e2e51603d409da09f93d1c060b5360e4b682e84
Maneesh Tewani <mtewani@google.com>
2025-03-28 13:47:15 -0700

    Fixed formatting

--- packages/react/src/data-connect/utils.ts
+++ packages/react/src/data-connect/utils.ts
@@ -1,29 +1,32 @@
 export function deepEqual(a: unknown, b: unknown) {
-  if(typeof a !== typeof b) {
+  if (typeof a !== typeof b) {
     return false;
   }
-  if(typeof a === 'object' && a !== null) {
-    if(Array.isArray(a)) {
-      if(a.length !== (b as unknown[]).length) {
+  if (typeof a === "object" && a !== null) {
+    if (Array.isArray(a)) {
+      if (a.length !== (b as unknown[]).length) {
         return false;
       }
       for (let index = 0; index < a.length; index++) {
         const elementA = a[index];
         const elementB = (b as unknown[])[index];
         const isEqual = deepEqual(elementA, elementB);
-        if(!isEqual) {
+        if (!isEqual) {
           return false;
         }
       }
       return true;
     }
     const keys = Object.keys(a);
-    if(keys.length !== Object.keys(b as object).length) {
+    if (keys.length !== Object.keys(b as object).length) {
       return false;
     }
-    for(const key of keys) {
-      const isEqual = deepEqual(a[key as keyof typeof a], (b as object)[key as keyof typeof b]);
-      if(!isEqual) {
+    for (const key of keys) {
+      const isEqual = deepEqual(
+        a[key as keyof typeof a],
+        (b as object)[key as keyof typeof b],
+      );
+      if (!isEqual) {
         return false;
       }
     }
