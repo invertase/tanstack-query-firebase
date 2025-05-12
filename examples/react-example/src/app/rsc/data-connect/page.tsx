d11f08dc62d750af75c7903fd822c4b4ee9a5450
Jacob Cable <jacobcable94@gmail.com>
2025-05-12 17:07:13 +0100

    ci: add turbo and intial run at release workflows

--- examples/react-example/src/app/rsc/data-connect/page.tsx
+++ examples/react-example/src/app/rsc/data-connect/page.tsx
@@ -1,14 +1,16 @@
 import { Movies } from "@/examples/data-connect";
 import { listMoviesRef } from "@dataconnect/default-connector";
-import { DataConnectQueryClient } from "@tanstack-query-firebase/react/data-connect";
+import { executeQuery } from "firebase/data-connect";
 import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/react-query";
 
 import "@/firebase";
 
 export default async function PostsPage() {
-	const queryClient = new DataConnectQueryClient();
-
-	await queryClient.prefetchDataConnectQuery(listMoviesRef());
+	const queryClient = new QueryClient();
+	const result = await executeQuery(listMoviesRef());
+	
+	queryClient.setQueryData([result.ref.name, result.ref.variables], result.data);
 
 	return (
 		<HydrationBoundary state={dehydrate(queryClient)}>
